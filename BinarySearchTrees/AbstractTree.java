2ee85e95ed9451594c14a8aa4ba9d8a19f19fdaf
Farkhod Abdukodirov <abdukodirovfarkhod@gmail.com>
2023-04-17 13:39:38 +0900

    Add files via upload

---
+++ BinarySearchTrees/AbstractTree.java
@@ -0,0 +1,20 @@
+package org.example.BinarySearchTrees;
+
+public abstract class AbstractTree<E> implements Tree<E>{
+    @Override
+    public void inorder(){
+
+    }
+    @Override
+    public void postorder(){
+
+    }
+    @Override
+    public void preorder(){
+
+    }
+    @Override
+    public boolean isEmpty(){
+        return getSize() == 0;
+    }
+}
